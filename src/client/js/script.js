2bdd6ba76c33d05a8861ea39ded7b84493ce2fda
tugberkugurlu <tugberkugurlu@gmail.com>
2014-09-28 17:32:36 +0300

    angular: first complex type binding

--- src/client/js/script.js
+++ src/client/js/script.js
@@ -1,3 +1,14 @@
 var MainTitleController = function($scope) {
     $scope.title = "Hello Bower, angularjs and other cool stuff...";
 }
+
+var MainController = function($scope) {
+    
+    var person = {
+        firstName: "Tugberk",
+        lastName: "Ugurlu",
+        imageSrc: "https://pbs.twimg.com/profile_images/418363635270823936/H_X0pAlb.jpeg"
+    };
+    
+    $scope.me = person;
+}
